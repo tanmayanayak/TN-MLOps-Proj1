d06bbd449d66389b184914539538661965b93017
Tanmaya Nayak <tanmaya.nayak.kumar@gmail.com>
2026-04-14 21:12:09 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tanmaya Nayak",
+    author_email="tanmaya.kumar700@gmail.com",
+    packages=find_packages()
+)
